08ca3a98a582406728c99e97d70145fc361399be
SurenaBot <104827062+hyno831@users.noreply.github.com>
2022-06-04 14:16:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['989389383634'],
+  ['989301860610', 'DEVIL', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ['989301860610'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -30,8 +29,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = '𝑆𝑈𝑅𝐸𝑁𝐴 ☆'
+global.author = '𝑅𝑂𝐵𝑂𝑇'
 
 global.multiplier = 69 // The higher, The harder levelup
 
